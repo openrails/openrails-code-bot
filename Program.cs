d839ee4936b356fb973e646c4f5f8053fe0c0b26
James Ross <git@james-ross.co.uk>
2019-06-30 10:20:33 +0100

    feat: Initial code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Rails_Code_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
